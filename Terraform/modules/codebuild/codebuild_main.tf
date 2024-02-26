57a900fef3f2b179e104472e4ae3c1a1675a02aa
Sam Duncan <samuel.p.duncan@gmail.com>
2024-02-26 16:42:42 -0500

    test-pipeline-->2

--- Terraform/modules/codebuild/codebuild_main.tf
+++ Terraform/modules/codebuild/codebuild_main.tf
@@ -17,11 +17,15 @@ resource "aws_codebuild_project" "test_build" {
     image                       = "aws/codebuild/amazonlinux2-x86_64-standard:4.0"
     type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "CODEBUILD"
+    environment_variable {
+      name  = "TERRAFORM_VERSION"
+      value = "1.7.3"
+    }
   }
 
   source {
-    type            = "GITHUB"
-    location        = "https://github.com/DuncanB112P/CICD-Pipeline_Duncan0224"
+    type            = "CODEPIPELINE"
+    buildspec = "./Terraform/files/buildspec.yml"
   }
 
   source_version = "main"
